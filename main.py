3b211eb14e74694b5858003c6a8f2647efbd15f2
Aykut Ulış <aykutulis@gmail.com>
2023-04-16 23:11:09 +0300

    refactor: router folder structure

--- main.py
+++ main.py
@@ -1,75 +1,9 @@
-from fastapi import FastAPI, Depends
-from models.TodoModel import TodoModel
-from schemas import CreateTodoSchema, UpdateTodoSchema
+from fastapi import FastAPI
 from database import Base
-from database import engine, SessionLocal
-from sqlalchemy.orm import Session
-from database import Base
-from exceptions.TodoNotFoundException import TodoNotFoundException
-
-app = FastAPI()
+from database import engine
+from routers import todo_router
 
 Base.metadata.create_all(bind=engine)
 
-
-def get_db():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
-
-
-@app.get("/todos")
-def get_all_todos(db: Session = Depends(get_db)):
-    return db.query(TodoModel).all()
-
-
-@app.get("/todos/{id}")
-def get_todo(id: int, db: Session = Depends(get_db)):
-    todo = db.query(TodoModel).filter(TodoModel.id == id).first()
-
-    if not todo:
-        raise TodoNotFoundException()
-
-    return todo
-
-
-@app.post("/todos")
-def create_todo(todo: CreateTodoSchema, db: Session = Depends(get_db)):
-    new_todo = TodoModel(**todo.dict())
-    db.add(new_todo)
-    db.commit()
-    db.refresh(new_todo)
-    return new_todo
-
-
-@app.put("/todos/{id}")
-def update_todo(id: int, todo: UpdateTodoSchema, db: Session = Depends(get_db)):
-    existing_todo = db.query(TodoModel).filter(TodoModel.id == id).first()
-
-    if not existing_todo:
-        raise TodoNotFoundException()
-
-    for key, value in todo.dict().items():
-        if value is not None:
-            setattr(existing_todo, key, value)
-
-    db.commit()
-
-    db.refresh(existing_todo)
-
-    return existing_todo
-
-
-@app.delete("/todos/{id}")
-def delete_todo(id: int, db: Session = Depends(get_db)):
-    todo = db.query(TodoModel).filter(TodoModel.id == id).first()
-
-    if not todo:
-        raise TodoNotFoundException()
-
-    db.delete(todo)
-    db.commit()
-
-    return {"message": "Todo deleted successfully"}
+app = FastAPI()
+app.include_router(todo_router)
---
+++ routers/todo_router.py
@@ -0,0 +1,67 @@
+from fastapi import APIRouter, Depends
+from models.TodoModel import TodoModel
+from schemas import CreateTodoSchema, UpdateTodoSchema
+from sqlalchemy.orm import Session
+from exceptions.TodoNotFoundException import TodoNotFoundException
+from utils.get_db import get_db
+
+router = APIRouter(
+    prefix="/todos",
+    tags=["todos"],
+    redirect_slashes=True,
+)
+
+
+@router.get("")
+def get_all_todos(db: Session = Depends(get_db)):
+    return db.query(TodoModel).all()
+
+
+@router.get("/{id}")
+def get_todo(id: int, db: Session = Depends(get_db)):
+    todo = db.query(TodoModel).filter(TodoModel.id == id).first()
+
+    if not todo:
+        raise TodoNotFoundException()
+
+    return todo
+
+
+@router.post("")
+def create_todo(todo: CreateTodoSchema, db: Session = Depends(get_db)):
+    new_todo = TodoModel(**todo.dict())
+    db.add(new_todo)
+    db.commit()
+    db.refresh(new_todo)
+    return new_todo
+
+
+@router.put("/{id}")
+def update_todo(id: int, todo: UpdateTodoSchema, db: Session = Depends(get_db)):
+    existing_todo = db.query(TodoModel).filter(TodoModel.id == id).first()
+
+    if not existing_todo:
+        raise TodoNotFoundException()
+
+    for key, value in todo.dict().items():
+        if value is not None:
+            setattr(existing_todo, key, value)
+
+    db.commit()
+
+    db.refresh(existing_todo)
+
+    return existing_todo
+
+
+@router.delete("/{id}")
+def delete_todo(id: int, db: Session = Depends(get_db)):
+    todo = db.query(TodoModel).filter(TodoModel.id == id).first()
+
+    if not todo:
+        raise TodoNotFoundException()
+
+    db.delete(todo)
+    db.commit()
+
+    return {"message": "Todo deleted successfully"}
