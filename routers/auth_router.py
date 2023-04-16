d5f4283e0cd6eb10370d4142f409b4ba40dc5af4
Aykut Ulış <aykutulis@gmail.com>
2023-04-17 00:25:45 +0300

    feat: login - WIP

--- routers/auth_router.py
+++ routers/auth_router.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Depends
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from schemas import CreateUserSchema
 from utils.database import get_db
+from utils.authenticate_user import authenticate_user
 from models import UserModel
 
 
@@ -19,3 +22,12 @@ def create_user(user: CreateUserSchema, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@router.post("/login")
+def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+    return user
